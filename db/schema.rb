dfaf48aa0cd355db6d252d6660dde95a28c285a5
ivan de martino <leandroivandm@gmail.com>
2023-03-16 10:54:22 +0000

    other touches

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_115325) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bucket_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "spot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_bucket_items_on_spot_id"
+    t.index ["user_id"], name: "index_bucket_items_on_user_id"
+  end
+
   create_table "favorite_folders", force: :cascade do |t|
     t.bigint "favorite_id", null: false
     t.bigint "folder_id", null: false
@@ -78,6 +87,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_115325) do
     t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "spot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_likes_on_spot_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "spots", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -110,5 +128,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_115325) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bucket_items", "spots"
+  add_foreign_key "bucket_items", "users"
   add_foreign_key "folders", "users"
+  add_foreign_key "likes", "spots"
+  add_foreign_key "likes", "users"
 end
